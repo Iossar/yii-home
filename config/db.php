1197f9740142792d67a4ccdee99adc0b840ee337
Iossar <kleymenov@save24.ru>
2019-12-11 15:12:01 +0300

    remote pull

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=i92901zk_yiihome',
+    'username' => 'i92901zk_yiihome',
+    'password' => '2468zzxx',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
